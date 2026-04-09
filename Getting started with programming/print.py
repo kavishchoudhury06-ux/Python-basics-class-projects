3f6e2e97131b1a0e5c654501931893e7d2f51cf8
kavishchoudhury06-ux <kavish.choudhury06@gmail.com>
2026-04-09 18:44:25 +0530

    Add files via upload

---
+++ Getting started with programming/print.py
@@ -0,0 +1,8 @@
+print("Kavish")
+a = 10
+b = 12
+print(60)
+print(4 % 2)
+print(a + b)
+print("Hello", end = ".")
+print(" \n English \n Science \n Mathematics \n")
---
+++ Getting started with programming/variables.py
@@ -0,0 +1,19 @@
+# DO
+a = 10
+
+_a = 11
+age = 11
+Age_Name = "Kavish11"
+NAME = "kAVISIH"
+naME1 = 6
+
+a = 11
+b = 10
+# Don't
+
+1name = 21
+-name = 1
+
+|ka = 11
+first name = 12
+
